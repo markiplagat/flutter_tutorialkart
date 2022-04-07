5147027becb1423e3c247fe139f5ebe350b283ea
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-04-07 17:36:40 +0300

    App bar

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = "My Tutorial App";
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatelessWidget(),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My tutorial. Am learning flutter'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello Flutter',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
