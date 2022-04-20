533b27e45c02682452dfa1952d7df0b01bf24f62
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-04-20 21:29:13 +0300

    Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,26 @@ State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hello world', style: TextStyle(fontSize: 30),)
+    return Scaffold(
+      appBar: AppBar(title: const Text("Column Widget")),
+      body: Center(
+        child: Column(
+          children: const <Widget>[
+            Text('Text 1', style: TextStyle(fontSize: 24.0),),
+            Text('Text 2', style: TextStyle(fontSize: 24.0),),
+            Icon(
+              Icons.beach_access,
+              color: Colors.pink,
+              size: 30.0,
+            ),
+            Icon(
+              Icons.audiotrack,
+              color: Colors.green,
+              size: 30.0,
+            ),
+          ],
+        )
+      )
     );
   }
 }
