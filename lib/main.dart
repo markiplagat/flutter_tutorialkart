ac3cb2aa281dc8640577be8d002d8e20b73af498
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-04-20 21:00:14 +0300

    Center widget

--- lib/main.dart
+++ lib/main.dart
@@ -29,74 +29,8 @@ State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          const SizedBox(height: 20,),
-          Container(
-            height: 150,
-            width: 150,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 5,
-                  spreadRadius: 1,
-                  offset: Offset(4, 4)
-                ),
-              ],
-              color: Colors.green[200],
-            ),
-            child: const Text('Container 1'),
-          ),
-          const SizedBox(height: 20,),
-          Container(
-            height: 150,
-            width: 150,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.blue,
-                  blurRadius: 3,
-                  spreadRadius: 5,
-                  offset: Offset(0, 0)
-                ),
-                BoxShadow(
-                  color: Colors.red,
-                  blurRadius: 5,
-                  spreadRadius: 1,
-                  offset: Offset(5, 5)
-                ),
-              ],
-              color: Colors.green[200],
-            ),
-            child: const Text('Container 2'),
-          ),
-          const SizedBox(height: 20,),
-          Container(
-            height: 150,
-            width: 150,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.deepPurpleAccent,
-                  blurRadius: 10,
-                  spreadRadius: 0,
-                  offset: Offset(0, 0)
-                ),
-              ],
-              color: Colors.green[200],
-            ),
-            child: const Text('Container 3')
-          )
-        ],
-      )
+    return const Center(
+      child: Text('Hello world', style: TextStyle(fontSize: 30),)
     );
   }
 }
