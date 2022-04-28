756278a953ef5df03e022e02c7f90415762cb2b5
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-04-28 09:45:38 +0300

    Data table

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,29 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: ListView(children: <Widget>[
+        DataTable(columns: const [
+          DataColumn(label: Text('No')),
+          DataColumn(label: Text('Name')),
+          DataColumn(label: Text('Class')),
+        ], rows: const [
+          DataRow(cells: [
+            DataCell(Text('1')),
+            DataCell(Text('Mark')),
+            DataCell(Text('8'))
+          ]),
+          DataRow(cells: [
+            DataCell(Text('2')),
+            DataCell(Text('John')),
+            DataCell(Text('9')),
+          ]),
+          DataRow(cells: [
+            DataCell(Text('3')),
+            DataCell(Text("Mr T")),
+            DataCell(Text('5'))
+          ])
+        ])
+      ]),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
