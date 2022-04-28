148cebb35cf984142b7938c80bd2af1ce3d55f5f
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-04-28 09:54:05 +0300

    Elevatedbutton with onPressed action

--- lib/main.dart
+++ lib/main.dart
@@ -30,29 +30,19 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: <Widget>[
-        DataTable(columns: const [
-          DataColumn(label: Text('No')),
-          DataColumn(label: Text('Name')),
-          DataColumn(label: Text('Class')),
-        ], rows: const [
-          DataRow(cells: [
-            DataCell(Text('1')),
-            DataCell(Text('Mark')),
-            DataCell(Text('8'))
-          ]),
-          DataRow(cells: [
-            DataCell(Text('2')),
-            DataCell(Text('John')),
-            DataCell(Text('9')),
-          ]),
-          DataRow(cells: [
-            DataCell(Text('3')),
-            DataCell(Text("Mr T")),
-            DataCell(Text('5'))
-          ])
-        ])
-      ]),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () { // Set (onPressed: null) to disable elevated button
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Good...You pressed me')));
+                },
+                child: const Text('Click Me')),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
